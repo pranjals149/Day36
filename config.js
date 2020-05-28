62d3b246a01eaa555b63f1c9a3249418419d2335
pranjals149 <42092291+pranjals149@users.noreply.github.com>
2020-05-28 12:26:20 +0530

    Day36

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secretKey': '1234-5678-9756',
+    'mongoUrl': 'mongo://localhost:27017/confusion'
+}
---
+++ routes/promoRouter.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Promotions = require('../models/promotions');
+var authenticate = require('../authenticate');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+    .get((req, res, next) => {
+        Promotions.find({})
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(authenticate.verifyUser, (req, res, next) => {
+        Promotions.create(req.body)
+        .then((promotion) => {
+            console.log('Promotions Created');
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotion);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .put(authenticate.verifyUser, (req, res, next) => {
+        res.end("Operation not supported");
+})
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        console.log('Deleting all Promotions');
+        Promotions.remove({})
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+});
+
+promoRouter.route('/:promoId')
+    .get((req, res, next) => {
+        Promotions.findById(req.params.promoId)
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .post(authenticate.verifyUser, (req, res, next) => {
+        res.end("POST operation not supported on promoId's: " + req.params.promoId);
+})
+    .put(authenticate.verifyUser, (req, res, next) => {
+        Promotions.findByIdAndUpdate(req.params.promoId, { $set: req.body }, { new: true })
+        .then((promotions) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(promotions);
+        }, (err) => next(err))
+        .catch((err) => next(err));
+})
+    .delete(authenticate.verifyUser, (req, res, next) => {
+        console.log("Deleting all Promotions");
+        Promotions.findByIdAndRemove(req.params.promoId)
+        .then((resp) => {
+            res.statusCode = 200;
+            res.setHeader = 'Content-Type', 'application/json';
+            res.json(resp);
+        }, (err) => next(err))
+        .catch((err) => next(err))
+})
+
+module.exports = promoRouter;
